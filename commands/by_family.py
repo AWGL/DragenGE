fcf8ed0c07ffe6c498546e5affe669c912c6844f
SophieS9 <sophie.shaw@wales.nhs.uk>
2022-06-24 15:01:04 +0100

    Updating to handle new Dragen outputs

--- commands/by_family.py
+++ commands/by_family.py
@@ -11,29 +11,47 @@ fam_dict = {}
 
 no_fam_int = 0
 
+#we need the metrics files (Dragen v3.7) and target bed coverage metrics files (Dragen v3.10.8) for excluding low coverage samples
 metrics_files = glob.glob('*/*.mapping_metrics.csv')
+target_files = glob.glob('*/*target_bed_coverage_metrics.csv')
 
 min_depth = 5
 
 sample_dict = {}
 
-for coverage_file in metrics_files:
+#Check if we need to be using metrics or target bed metrics file for depth, using just the first file
+if 'Average sequenced coverage over target region' in open(metrics_files[0]).read():
+	for coverage_file in metrics_files:
 
-	with open(coverage_file) as csvfile:
-		spamreader = csv.reader(csvfile, delimiter=',')
-		for row in spamreader:
-			key = row[2]
-			value = row[3]
-			if key == 'Average sequenced coverage over target region':
-				if float(value) > min_depth:
-					sample_id = coverage_file.split('/')[0]
-					sample_dict[sample_id] = sample_id
-					break
+		with open(coverage_file) as csvfile:
+			spamreader = csv.reader(csvfile, delimiter=',')
+			for row in spamreader:
+				key = row[2]
+				value = row[3]
+				if key == 'Average sequenced coverage over target region':
+					if float(value) > min_depth:
+						sample_id = coverage_file.split('/')[0]
+						sample_dict[sample_id] = sample_id
+						break
+else:
+	for coverage_file in target_files:
+
+                with open(coverage_file) as csvfile:
+                        spamreader = csv.reader(csvfile, delimiter=',')
+                        for row in spamreader:
+                                key = row[2]
+                                value = row[3]
+                                if key == 'Average alignment coverage over target region':
+                                        if float(value) > min_depth:
+                                                sample_id = coverage_file.split('/')[0]
+                                                sample_dict[sample_id] = sample_id
+                                                break
+
 
 with open(ped) as csvfile:
 	spamreader = csv.reader(csvfile, delimiter='\t')
 	for row in spamreader:
-
+		
 		fam_id = row[0]
 		sample_name = row[1]
 
